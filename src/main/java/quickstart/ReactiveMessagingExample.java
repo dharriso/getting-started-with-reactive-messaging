149e404bc4fb91b8edbbe88aa29085134315d09a
dharriso <declan.harrison@optiva.com>
2024-04-11 22:32:02 +0100

    wee test

--- src/main/java/quickstart/ReactiveMessagingExample.java
+++ src/main/java/quickstart/ReactiveMessagingExample.java
@@ -8,22 +8,22 @@ import org.eclipse.microprofile.reactive.messaging.Outgoing;
 @ApplicationScoped
 public class ReactiveMessagingExample {
 
-    @Outgoing("data")
+    @Outgoing("source")
     public Multi<String> source() {
         return Multi.createFrom().items("hello", "from", "SmallRye", "reactive", "messaging");
     }
 
-    @Incoming("source")
-    @Outgoing("processed-a")
-    public String toUpperCase(String payload) {
-        return payload.toUpperCase();
-    }
+    //@Incoming("source")
+    //@Outgoing("processed-a")
+    //public String toUpperCase(String payload) {
+    //    return payload.toUpperCase();
+   /// }
 
-    @Incoming("processed-a")
-    @Outgoing("processed-b")
-    public Multi<String> filter(Multi<String> input) {
-        return input.select().where(item -> item.length() > 4);
-    }
+    //@Incoming("processed-a")
+    //@Outgoing("processed-b")
+    //public Multi<String> filter(Multi<String> input) {
+      //  return input.select().where(item -> item.length() > 4);
+    //}
 
     @Incoming("processed-b")
     public void sink(String word) {
