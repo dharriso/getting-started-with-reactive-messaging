b2834be5923182e438986411750d31fce9a9a0a8
dharriso <declan.harrison@optiva.com>
2024-04-11 21:52:23 +0100

    wee test

---
+++ src/main/java/quickstart/ReactiveMessagingExample.java
@@ -0,0 +1,33 @@
+package quickstart;
+
+import io.smallrye.mutiny.Multi;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+@ApplicationScoped
+public class ReactiveMessagingExample {
+
+    @Outgoing("data")
+    public Multi<String> source() {
+        return Multi.createFrom().items("hello", "from", "SmallRye", "reactive", "messaging");
+    }
+
+    @Incoming("source")
+    @Outgoing("processed-a")
+    public String toUpperCase(String payload) {
+        return payload.toUpperCase();
+    }
+
+    @Incoming("processed-a")
+    @Outgoing("processed-b")
+    public Multi<String> filter(Multi<String> input) {
+        return input.select().where(item -> item.length() > 4);
+    }
+
+    @Incoming("processed-b")
+    public void sink(String word) {
+        System.out.println(">> " + word);
+    }
+
+}
